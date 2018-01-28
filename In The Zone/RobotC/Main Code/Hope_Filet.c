160d296b53c566c7ebccf8949401d932ce185289
Logan <wolvie2013@gmail.com>
2018-01-27 19:48:56 -0500

    Competition Update
    - Added baseline code for LCD display and Autonomous selection.
    
    - Created autonomous test functions

--- In The Zone/RobotC/Main Code/Hope_Filet.c
+++ In The Zone/RobotC/Main Code/Hope_Filet.c
@@ -39,7 +39,7 @@ void pre_auton()
   // running between Autonomous and Driver controlled modes. You will need to
   // manage all user created tasks if set to false.
   bStopTasksBetweenModes = true;
-
+  bLCDBacklight = true;
 	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
 	// used by the competition include file, for example, you might want
 	// to display your team name on the LCD in this function.
@@ -58,14 +58,26 @@ void pre_auton()
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-
+int autover;
 task autonomous()
 {
-	/* Autonomous plan: TBD
-	 *
-	*/
-  // Remove this function call once you have "real" code.
-  AutonomousCodePlaceholderForTesting();
+	if(autover == 1) {
+		motor[LeftMid] = 127;
+		motor[LeftSides] = 127;
+		motor[RightMid] = 127;
+		motor[RightSides] = 127;
+		delay(2000);
+	}
+	//Simple test function for autonomous until actual code can be created
+
+	if(autover == 2) {
+		motor[LeftMid] = 127;
+		motor[LeftSides] = 127;
+		motor[RightMid] = -127;
+		motor[RightSides] = -127;
+		delay(2000);
+	}
+	//Testing autonomous selection
 }
 
 /*---------------------------------------------------------------------------*/
@@ -80,7 +92,9 @@ task autonomous()
 
 task usercontrol()
 {
-  // User control code here, inside the loop
+  //Plans for this bot:
+	//	- LCD Display, will require convincing of Nick
+	//	- Quad Encoders, will require further testing before implementation
 
   while (true)
   {
@@ -111,5 +125,42 @@ task usercontrol()
 
 		//Mobile Goal Pickup
 		motor[MobileGoal] = vexRT[Btn5D]*-127 + vexRT[Btn5U]*127; //Pickup control using left bumpers
+
+		//---- Autonomous Test Function ---------------------------------------//
+		if(vexRT[Btn8U] == 1) { //If both buttons 8U and 7U are pressed simultaneously, run the autonomous() task
+			if(vexRT[Btn7U] == 1) {
+				startTask(autonomous); //Could possibly shut us out of usercontrol(), would require reboot
+			}
+		}
+
+		//---- LCD Display Test -----------------------------------------------//
+		float batlevel = nAvgBatteryLevel/1000;
+		//Sets batlevel to the battery level
+
+		int btnPressed = nLCDButtons;
+
+		clearLCDLine(0);
+		clearLCDLine(1);
+		//Resets LCD Display
+
+		displayLCDString(0, 1, "1264B Battery: ");
+		displayLCDNumber(0, 17, batlevel);
+		//Displays battery level on the top line
+
+		displayLCDString(1, 1, "Autonomous Version: ");
+		displayLCDNumber(1, 21, autover);
+		autover = 1;
+		//Setup for Autonomous selection
+
+		if(btnPressed == 1) { //If the left button is pressed, scroll left on autonomous versions
+			autover = autover - 1;
+			delay(200); //Buffer for time
+		}
+
+		if(btnPressed == 4) { //If the right button is pressed, scroll right on autonomous versions
+			autover = autover + 1;
+			delay(200); //Buffer for time
+		}
+
   }
 }
